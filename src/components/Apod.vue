a44eea0b156d516db0a655c4ff59e7ff533b8ef0
wyattDockstader <81665137+wyattDockstader@users.noreply.github.com>
2021-06-08 16:04:09 -0600

    added text

--- src/components/Apod.vue
+++ src/components/Apod.vue
@@ -1,6 +1,14 @@
 <template>
-  <div v-if="state.pic">
-    <img :src="apod.url" alt="this broken man">
+  <div class="container-fluid bg-dark text-light">
+    <div class="row">
+      <div class="col-8 p-3" v-if="state.pic">
+        <img :src="apod.url" alt="this broken man">
+      </div>
+      <div class="col-4 p-3">
+        <h3>{{ apod.title }}</h3>
+        <p>{{ apod.explination }}</p>
+      </div>
+    </div>
   </div>
 </template>
 <script>
